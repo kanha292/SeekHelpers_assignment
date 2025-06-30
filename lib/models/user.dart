77bb0398d1b14bdb0c2f4f626edabd71691a0d45
kanhaiya bishnoi <119165054+kanha292@users.noreply.github.com>
2025-07-01 02:44:58 +0530

    Update user.dart

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -3,21 +3,35 @@ class User {
   final String name;
   final String email;
   final String phone;
+  final String username;
+  final String address;
+  final String company;
 
   User({
     required this.id,
     required this.name,
     required this.email,
     required this.phone,
+    required this.username,
+    required this.address,
+    required this.company,
   });
 
-  // Factory method to create User object from JSON
   factory User.fromJson(Map<String, dynamic> json) {
+    final addressJson = json['address'] ?? {};
+    final companyJson = json['company'] ?? {};
+
+    String fullAddress =
+        "${addressJson['street'] ?? ''}, ${addressJson['suite'] ?? ''}, ${addressJson['city'] ?? ''} - ${addressJson['zipcode'] ?? ''}";
+
     return User(
       id: json['id'],
       name: json['name'],
       email: json['email'],
       phone: json['phone'],
+      username: json['username'] ?? '',
+      address: fullAddress,
+      company: companyJson['name'] ?? '',
     );
   }
 }
